8c0e08873eb3633f85b6066047d3cd099123a1cd
mayados <mariannedos.cours@gmail.com>
2023-11-14 16:57:29 +0100

    add condition for displayNavList() and add function displayUserParameters()

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+if (window.innerWidth < 769){
 /* Display nav links on mobiles and pads */
 
 function displayNavList() {
@@ -8,19 +9,36 @@ function displayNavList() {
     burger.addEventListener('click', (event) => {    
       navList.classList.toggle('show-list');
     });    
-  } 
+  }   
+}
+
   
  displayNavList();
 
  /* Display accordeon menu responsive annonces */
  function displayAnnonces() {
-  const categories = document.querySelector('.na__list');
-  const linkAnnonces = document.querySelector('.na__dropdown_annonces');
-  console.log(burger);
+  const categories = document.querySelector('.na__dropdown_annonces');
+  const linkAnnonces = document.querySelector('.link-annonces');
+  console.log(linkAnnonces);
 
-  burger.addEventListener('click', (event) => {    
-    categories.classList.toggle('show-categories');
+  linkAnnonces.addEventListener('click', (event) => {    
+    console.log("click");
+    categories.classList.toggle('na__dropdown_annonces--active');
   });    
 } 
 
 displayAnnonces();
+
+ /* Display accordeon menu responsive user parameters */
+ function displayUserParameters() {
+  const user = document.querySelector('.user-link');
+  const dropdownUser = document.querySelector('.na__dropdown_user');
+  console.log(dropdownUser);
+
+  user.addEventListener('click', (event) => {    
+    console.log("click");
+    dropdownUser.classList.toggle('na__dropdown_user--active');
+  });    
+} 
+
+displayUserParameters();
