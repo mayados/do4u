3f34bee143e0c274e558e57dde93fa651b98a581
mayados <mariannedos.cours@gmail.com>
2023-11-17 13:32:13 +0100

    add function when window is resized

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,56 @@
-if (window.innerWidth < 769){
-/* Display nav links on mobiles and pads */
+function displayMobileVue() {
 
-function displayNavList() {
-    const navList = document.querySelector('.na__list');
-    const burger = document.querySelector('.na__burger-menu');
-    console.log(burger);
-  
-    burger.addEventListener('click', (event) => {    
-      navList.classList.toggle('show-list');
+  if (window.innerWidth < 769){
+  /* Display nav links on mobiles and pads */
+
+  function displayNavList() {
+      const navList = document.querySelector('.na__list');
+      const burger = document.querySelector('.na__burger-menu');
+      console.log(burger);
+    
+      burger.addEventListener('click', (event) => {    
+        navList.classList.toggle('show-list');
+      });    
+    } 
+    
+  displayNavList();
+
+
+  /* Display accordeon menu responsive annonces */
+  function displayAnnonces() {
+    const categories = document.querySelector('.na__dropdown_annonces');
+    const linkAnnonces = document.querySelector('.link-annonces');
+    console.log(linkAnnonces);
+
+    linkAnnonces.addEventListener('click', (event) => {    
+      console.log("click");
+      categories.classList.toggle('na__dropdown_annonces--active');
     });    
   } 
-  
-  
- displayNavList();
 
+  displayAnnonces();
 
- /* Display accordeon menu responsive annonces */
- function displayAnnonces() {
-  const categories = document.querySelector('.na__dropdown_annonces');
-  const linkAnnonces = document.querySelector('.link-annonces');
-  console.log(linkAnnonces);
+  /* Display accordeon menu responsive user parameters */
+  function displayUserParameters() {
+    const user = document.querySelector('.user-link');
+    const dropdownUser = document.querySelector('.na__dropdown_user');
+    console.log(dropdownUser);
 
-  linkAnnonces.addEventListener('click', (event) => {    
-    console.log("click");
-    categories.classList.toggle('na__dropdown_annonces--active');
-  });    
-} 
+    user.addEventListener('click', (event) => {    
+      console.log("click");
+      dropdownUser.classList.toggle('na__dropdown_user--active');
+    });    
+  } 
 
-displayAnnonces();
+  displayUserParameters(); 
 
- /* Display accordeon menu responsive user parameters */
- function displayUserParameters() {
-  const user = document.querySelector('.user-link');
-  const dropdownUser = document.querySelector('.na__dropdown_user');
-  console.log(dropdownUser);
-
-  user.addEventListener('click', (event) => {    
-    console.log("click");
-    dropdownUser.classList.toggle('na__dropdown_user--active');
-  });    
-} 
-
-displayUserParameters(); 
+  }
 
 }
+
+// When the window is resized, we call the displayMobileVue function to have a functionnal burger menu etc on mobile version
+window.onresize = displayMobileVue;
+
 
 
 
