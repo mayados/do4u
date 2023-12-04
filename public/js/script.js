6e8d6f41f9f95e4b56a04429767e23ba9db0dea7
sev-campus <sevilia.seitvaliieva@ccicampus.fr>
2023-12-04 11:13:00 +0100

    added btn voir plus for annonce description

--- public/js/script.js
+++ public/js/script.js
@@ -142,3 +142,19 @@ if(signalements){
     });
   });  
 }
+
+function voirPlus() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Voir plus"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Voir moins"; 
+    moreText.style.display = "inline";
+  }
+}
