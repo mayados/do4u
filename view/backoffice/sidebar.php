812076ec5903b2e66d17c26b0cdef06a980f4538
Zara-ahmadi <ahmadizahra897@gmail.com>
2023-12-16 20:13:02 +0100

    add the all pages to the backoffice

---
+++ view/backoffice/sidebar.php
@@ -0,0 +1,69 @@
+<section id="sidebar" class="col-lg-3">
+    <a href="#" class="brand d-flex justify-content-center pt-lg-2">
+    <h1 class="fw-bolder small">Do4U</h1>
+    </a>
+    <ul class="side-menu top ps-0">
+    <li class="">
+        <a href="index.html" class="icon-sidebar">
+        <i class="bx bxs-dashboard"></i>
+        <span class="text side-text">Backoffice</span>
+        </a>
+    </li>
+    <li>
+        <a href="utilisatuer.html">
+        <i class="bx bx-group"></i>
+        <span class="text">Utilisatuer</span>
+        </a>
+    </li>
+    <li>
+        <a href="annonce.html">
+        <i class="bx bxs-chart"></i>
+        <span class="text">Annonces</span>
+        </a>
+    </li>
+    <li>
+        <a href="categorie.html">
+        <i class="bx bx-chart"></i>
+        <span class="text">Catégorie</span>
+        </a>
+    </li>
+    <li>
+        <a href="typeAnnoce.html">
+        <i class="bx bx-doughnut-chart"></i>
+        <span class="text">TypeAnnonce</span>
+        </a>
+    </li>
+    <li>
+        <a href="conversation.html">
+        <i class="bx bx-message-dots"></i>
+        <span class="text">Conversation</span>
+        </a>
+    </li>
+    <li>
+        <a href="avis.html">
+        <i class="bx bx-conversation"></i>
+        <span class="text">Avis</span>
+        </a>
+    </li>
+    <li>
+        <a href="signalement.html">
+        <i class="bx bx-conversation"></i>
+        <span class="text">Signalement</span>
+        </a>
+    </li>
+    </ul>
+    <ul class="side-menu ps-0">
+    <li>
+        <a href="#">
+        <i class="bx bx-cog"></i>
+        <span class="text">Paramètres</span>
+        </a>
+    </li>
+    <li>
+        <a href="#" class="logout">
+        <i class="bx bx-log-out-circle"></i>
+        <span class="text">Déconnexion</span>
+        </a>
+    </li>
+    </ul>
+</section>
