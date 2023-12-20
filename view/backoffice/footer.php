245f28f81bfdac371e7d372aa7b224d77b41f904
Zara-ahmadi <ahmadizahra897@gmail.com>
2023-12-20 20:58:45 +0100

    rename 7 files, update 1 file and delete 1 file

---
+++ view/backoffice/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer d-footer mt-auto">
+        <div class="d-flex align-items-center justify-content-center">
+        <p class="small text-center text-light pt-2">Do4U - 2023</p>
+        </div>
+    </div>
+</footer>
