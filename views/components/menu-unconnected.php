a3e95fe8eb8b666ac0328c57e49f57acf77923a9
Zara-ahmadi <ahmadizahra897@gmail.com>
2024-01-04 11:26:52 +0100

    update 8 files

--- views/components/menu-unconnected.php
+++ views/components/menu-unconnected.php
@@ -15,7 +15,7 @@
             </form>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasEndLabel">
                 <div class="offcanvas-header">
-                <h2 class="offcanvas-title" id="offcanvasEndLabel"><a href="index.php"><img src="../public/img/logo_do4u.svg" alt="logo Do4U" class="logo"></a></h2>
+                <h2 class="offcanvas-title" id="offcanvasEndLabel"><a href="index.php"><img src="../public/assets/img/logo_do4u.svg" alt="logo Do4U" class="logo"></a></h2>
                 <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body small">
