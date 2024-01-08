5c5058d351de139c5ba1637131a88722bf22c870
Zara-ahmadi <ahmadizahra897@gmail.com>
2024-01-08 14:29:02 +0100

    add the header and footer by php

--- public/contact.php
+++ public/contact.php
@@ -1,12 +1,19 @@
 <?php
-require_once __DIR__.'/../bootstrap/app.php';
+require_once __DIR__ . '/../vendor/autoload.php'; // Include autoloader if using Composer
+use helpers\class\App;
+use Controllers\ContactController;
+use Controllers\ComponentController;
 
-// Check only if guest
-Auth::isGuestOrRedirect();
+// Check only if guest 
+// should be solve the error Auth is not recognized
+// Auth::isGuestOrRedirect();
 
-require_once base_path('Controllers/HomeController.php');
-$controller = new Controllers\HomeController();
-$controller->showContactPage();
+$componentController = new ComponentController();
+
+$adsController = new ContactController($componentController);
+$adsController->showMenu();
+$adsController->showContactPage();
+$adsController->showFooter();
 
 // Remove errors, success and old data
 App::terminate();
