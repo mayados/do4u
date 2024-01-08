923664341104b80bd72519abc0f383ee5a1ad8ca
Zara-ahmadi <ahmadizahra897@gmail.com>
2024-01-08 14:34:52 +0100

    create 1 file and update 2 files | header and footer added to the conversation view

---
+++ Controllers/ConversationController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Controllers;
+use Controllers\ComponentController;
+
+class ConversationController
+{
+    // const URL_CREATE = '/views/creationAd.php';
+    // const URL_INDEX = '/views/index.php';
+    // const URL_HANDLER = '/handlers/ad-handler.php';
+    private $componentController;
+
+    public function __construct(ComponentController $componentController) {
+        $this->componentController = $componentController;
+    }
+    public function showMyProfile() {
+        require_once __DIR__ . '/../views/conversation.php';
+    }
+    public function showMenu() {
+        
+        $this->componentController->renderHeader();
+        
+    }
+    public function showFooter(){
+        $this->componentController->renderFooter();
+    } 
+}
